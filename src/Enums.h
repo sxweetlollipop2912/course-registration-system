53bf083f1db574c3a1b0bb19df547e7c4deed696
sxweetlollipop2912 <thaolygoat@gmail.com>
2022-04-22 00:15:56 +0700

    Enum UserType, Gender to string.

--- src/Enums.h
+++ src/Enums.h
@@ -12,17 +12,21 @@ enum class DataType {
 };
 
 enum class UserType {
-    Staff,
-    Student,
-    Unknown
+    Staff = 0,
+    Student = 1,
+    Unknown = 2
 };
+/// Usage: userTypeStr[(int)UserType::Unknown]
+const string userTypeStr[] = { "Staff","Student","Unknown" };
 
 enum class Gender {
-    Male,
-    Female,
-    Other,
-    Unknown
+    Male = 0,
+    Female = 1,
+    Other = 2,
+    Unknown = 3
 };
+/// Usage: genderStr[(int)Gender::Unknown]
+const string genderStr[] = { "Male","Female","Other","Unknown" };
 
 struct FullName {
     string first;
