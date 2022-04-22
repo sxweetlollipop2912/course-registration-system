40cee7d112481af9935f2f67389305bd08943cd5
HuyNVD <101879720+HuyNVD@users.noreply.github.com>
2022-04-22 23:23:47 +0700

    add scene 9

---
+++ src/GUI/Scene9.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Interaction.h"
+
+void go_back(int dummy);
+void go_to_scene10(int dummy);
+void go_to_scene11(int dummy);
+void scene9(sf::RenderWindow& window, App &_app);
