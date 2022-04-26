c5690f50c40cc43436ba3a7dc77aba7408e44ea0
HuyNVD <101879720+HuyNVD@users.noreply.github.com>
2022-04-26 14:36:11 +0700

    reupload

--- src/App/RegistrationSession.cpp
+++ src/App/RegistrationSession.cpp
@@ -3,7 +3,23 @@
 #include "RegistrationSession.h"
 #include "Utils.h"
 
-bool RegistrationSession::isOngoing() {
+bool RegistrationSession::isOngoing() const {
+	return !hasEnded() && !hasNotStarted();
+}
+
+bool RegistrationSession::hasInit() const {
+	auto st_tmp = start, en_tmp = end;
+	return mktime(&st_tmp) != -1 && mktime(&en_tmp) != -1;
+}
+
+bool RegistrationSession::hasEnded() const {
+	auto now = Utils::now();
+	auto en_tmp = end;
+	return mktime(&en_tmp) <= mktime(&now);
+}
+
+bool RegistrationSession::hasNotStarted() const {
     auto now = Utils::now();
-    return mktime(&start) <= mktime(&now) && mktime(&end) > mktime(&now);
+	auto st_tmp = start;
+	return mktime(&st_tmp) > mktime(&now);
 }
