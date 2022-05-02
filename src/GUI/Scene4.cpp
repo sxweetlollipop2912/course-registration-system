9bc46941e1ef63cb1b3fb519499494ce86ed914c
nghia <nghia051>
2022-05-02 22:51:52 +0700

    fix scene 4

--- src/GUI/Scene4.cpp
+++ src/GUI/Scene4.cpp
@@ -1,5 +1,8 @@
 #include "Scene4.h"
 
+#include<iostream>
+using namespace std;
+
 static App* app;
 static sf::RenderWindow* windowP;
 static bool inCreate;
@@ -14,6 +17,23 @@ Input_Textbox* yearInputBoxP;
 
 DataIter* clP;
 
+static string double_to_string(const double& val, int d = 2) {
+	std::stringstream stream;
+	stream << std::fixed << std::setprecision(d) << val;
+	return stream.str();
+}
+
+static string int_to_string(int x)
+{
+	string res = "";
+	while (x > 0)
+	{
+		res.push_back(char(x % 10 + 48));
+		x /= 10;
+	}
+	reverse(res.begin(), res.end());
+	return res;
+}
 static int to_int(string s)
 {
 	int sum = 0;
@@ -222,6 +242,192 @@ static void remove_class()
 	app->deleteClass(*clP);
 }
 
+void draw_score(List<DataIter>& students, List<Course>& allCourse, int numPage, int totalPage)
+{
+	Textbox studentBox("", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(0, 0), sf::Vector2f(350, 50), sf::Color::Transparent);
+	Textbox studentIdBox("", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(0, 0), sf::Vector2f(150, 50), sf::Color::Transparent);
+	studentBox.set_outline(sf::Color::Black);
+	studentIdBox.set_outline(sf::Color::Black);
+
+	
+	Textbox nameTitleBox("Name", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(150, 100), sf::Vector2f(350, 50), sf::Color::Transparent);
+	Textbox idTitleBox("Id", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(0, 100), sf::Vector2f(150, 50), sf::Color::Transparent);
+	nameTitleBox.set_outline(sf::Color::Black);
+	idTitleBox.set_outline(sf::Color::Black);
+	nameTitleBox.draw(*windowP, app->default_font);
+	idTitleBox.draw(*windowP, app->default_font);
+
+	int cnt = 1;
+	students.for_each([&](DataIter& stu)
+		{
+			auto ptr = stu.ptr<Student>();
+			studentBox.set_text(ptr->name.toStr());
+			studentBox.set_box_position(sf::Vector2f(150, 100 + cnt * 50));
+
+			studentIdBox.set_text(ptr->student_id);
+			studentIdBox.set_box_position(sf::Vector2f(0, 100 + cnt * 50));
+			cnt++;
+
+			studentBox.draw(*windowP, app->default_font);
+			studentIdBox.draw(*windowP, app->default_font);
+		});
+	cnt = 0;
+	Textbox courseBox("", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(0, 0), sf::Vector2f(150, 50), sf::Color::Transparent);
+	courseBox.set_outline(sf::Color::Black);
+	int indexCourse = 0;
+	allCourse.for_each([&](Course& cour)
+		{
+			if (indexCourse >= (numPage - 1) * 4 && indexCourse < numPage * 4)
+			{
+				courseBox.set_text(cour.id);
+				courseBox.set_box_position(sf::Vector2f(500 + cnt * 150, 100));
+				courseBox.draw(*windowP, app->default_font);
+				int indexStudent = 0;
+				students.for_each([&](DataIter& stu)
+					{
+						auto studentPtr = stu.ptr<Student>();
+						auto scorePtr = studentPtr->getScore(cour.uid);
+						if (scorePtr == NULL)
+						{
+							courseBox.set_text("X");
+						}
+						else
+						{
+							courseBox.set_text(double_to_string(scorePtr->total));
+						}
+						courseBox.set_box_position(sf::Vector2f(500 + cnt * 150, 150 + indexStudent * 50));
+						courseBox.draw(*windowP, app->default_font);
+						indexStudent++;
+					});
+				cnt++;
+			}
+			indexCourse++;
+		});
+
+
+	if (numPage != totalPage) return;
+	courseBox.set_text("GPA");
+	courseBox.set_box_position(sf::Vector2f(500 + cnt * 150, 100));
+	courseBox.draw(*windowP, app->default_font);
+	int indexStudent = 0;
+	students.for_each([&](DataIter& stu)
+		{
+			auto studentPtr = stu.ptr<Student>();
+			List<DataIter> courses = studentPtr->courses;
+			double sumScore = 0.0, sumCre = 0;
+			courses.for_each([&](DataIter& cour)
+				{
+					auto coursePtr = cour.ptr<Course>();
+					sumScore += (studentPtr->getScore(coursePtr->uid)->total) * (double)(coursePtr->credits);
+					sumCre += coursePtr->credits;
+				});
+			if (sumCre == 0)
+			{
+				courseBox.set_text("!");
+			}
+			else courseBox.set_text(double_to_string(sumScore / sumCre));
+			courseBox.set_box_position(sf::Vector2f(500 + cnt * 150, 150 + indexStudent * 50));
+			courseBox.draw(*windowP, app->default_font);
+			indexStudent++;
+		});
+			
+		
+}
+
+static void view_score()
+{
+	auto classPtr = clP->ptr<Class>();
+	List<DataIter> students = classPtr->students;
+	List<Course> allCourse;
+	students.for_each([&](DataIter& stu)
+		{
+			auto studentPtr = stu.ptr<Student>();
+			List<DataIter> courses = studentPtr->courses;
+			courses.for_each([&](DataIter& cour)
+				{
+					auto coursePtr = cour.ptr<Course>();
+					allCourse.push_back(*coursePtr);
+				});
+		});
+	
+	allCourse.unique();
+	
+	sf::Texture texture;
+	texture.loadFromFile("assets/images/go_back.png");
+	Button_Sprite back_button = Button_Sprite(texture, sf::Vector2f(10, 5), sf::Vector2f(40, 40));
+
+	texture.loadFromFile("assets/images/previous.jpg");
+	Button_Sprite previous_button(texture, sf::Vector2f(910, 50), sf::Vector2f(40, 40));
+
+	texture.loadFromFile("assets/images/next.jpg");
+	Button_Sprite next_button(texture, sf::Vector2f(950, 50), sf::Vector2f(40, 40));
+
+	int numPage = 1;
+	int totalPage = (allCourse.size() + 1 + 3) / 4;
+	Textbox numPageBox(int_to_string(numPage) + "/" + int_to_string(totalPage), defaultMediumCharSize, sf::Color::Black, sf::Vector2f(1000, 40), sf::Vector2f(50, 50), sf::Color::Transparent);
+	while (windowP->isOpen())
+	{
+		sf::Event event;
+		sf::Vector2i mousePos = Mouse::getPosition(*windowP);
+		while (windowP->pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+			{
+				windowP->close();
+				app->scenes.clear();
+			}
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					if (mousePos.x >= 10 && mousePos.x <= 50 && mousePos.y >= 5 && mousePos.y <= 45)
+					{
+						return;
+					}
+					if (previous_button.inside(mousePos.x, mousePos.y))
+					{
+						numPage = max(numPage - 1, 1);
+						numPageBox.set_text(int_to_string(numPage) + "/" + int_to_string(totalPage));
+					}
+					if (next_button.inside(mousePos.x, mousePos.y))
+					{
+						numPage = min(numPage + 1, totalPage);
+						numPageBox.set_text(int_to_string(numPage) + "/" + int_to_string(totalPage));
+					}
+				}
+			}
+		}
+		windowP->clear(sf::Color::White);
+		if (mousePos.x >= 10 && mousePos.x <= 50 && mousePos.y >= 5 && mousePos.y <= 45)
+		{
+			back_button.idle = false;
+		}
+		else back_button.idle = true;
+		back_button.draw(*windowP, app->default_font);
+
+		if (previous_button.inside(mousePos.x, mousePos.y))
+		{
+			previous_button.idle = false;
+		}
+		else previous_button.idle = true;
+		previous_button.draw(*windowP, app->default_font);
+		
+		if (next_button.inside(mousePos.x, mousePos.y))
+		{
+			next_button.idle = false;
+		}
+		else next_button.idle = true;
+		next_button.draw(*windowP, app->default_font);
+
+		numPageBox.draw(*windowP, app->default_font);
+
+		draw_score(students, allCourse, numPage, totalPage);
+
+		windowP->display();
+	}
+}
+
 void scene4(sf::RenderWindow& window, App& _app, DataIter &cl)
 {
 	windowP = &window;
@@ -231,7 +437,7 @@ void scene4(sf::RenderWindow& window, App& _app, DataIter &cl)
 	Textbox headText("Student list", 40, sf::Color::Black, sf::Vector2f(windowWidth / 3 - 200, 100), sf::Vector2f(200, 50), sf::Color::Transparent);
 	Textbox headText2("Functions", 40, sf::Color::Black, sf::Vector2f(windowWidth / 3 * 2, 100), sf::Vector2f(200, 50), sf::Color::Transparent);
 
-	Textbox addStudentButton("Add student", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(windowWidth / 3 * 2 - 50, 100 + 75), sf::Vector2f(300, 50), sf::Color::Green);
+	Textbox addStudentButton("Add a student", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(windowWidth / 3 * 2 - 50, 100 + 75), sf::Vector2f(300, 50), sf::Color::Green);
 	Textbox importStudentButton("Import student", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(windowWidth / 3 * 2 - 50, 100 + 150), sf::Vector2f(300, 50), sf::Color::Green);
 	Textbox removeStudentButton("Remove student by id", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(windowWidth / 3 * 2 - 50, 100 + 225), sf::Vector2f(300, 50), sf::Color::Green);
 	Textbox viewScoreButton("View score board", defaultMediumCharSize, sf::Color::Black, sf::Vector2f(windowWidth / 3 * 2 - 50, 100 + 300), sf::Vector2f(300, 50), sf::Color::Green);
@@ -272,6 +478,10 @@ void scene4(sf::RenderWindow& window, App& _app, DataIter &cl)
 					if (removeStudentButton.inside(mousePos.x, mousePos.y))
 					{
 						revome_student();
+					}
+					if (viewScoreButton.inside(mousePos.x, mousePos.y))
+					{
+						view_score();
 					}
 					if (removeClassButton.inside(mousePos.x, mousePos.y))
 					{
